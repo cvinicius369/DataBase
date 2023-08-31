744ad13a0caf8d6f9e7b83b191666afc9018ee89
Caio Vinicius <137227050+cvinicius369@users.noreply.github.com>
2023-08-31 10:48:22 -0300

    Update main.py

--- Projeto DataHome/main.py
+++ Projeto DataHome/main.py
@@ -1,15 +1,22 @@
+#Coded by: Caio Vinicius (cvinicius369)
+#E-mail: vinicius182102@gmail.com
+#Project: Database in Python
+#Importações
 import database
 import datetime as dt
 import time
 
+#Definição do horario em que iniciou o sistema
 hoje = dt.datetime.today()
 
+#Menu
 print("-------------------------------------------------------------")
 print(hoje)
 print("-------------------------------------------------------------")
 print("1- Cadastros\n2- Despesas")
 act = int(input("R: "))
 
+#Chamado das classes em database.py permitindo ter acesso ao gerenciamento do banco de dados
 if act == 1:
     print("-------------------------------------------------------------")
     database.FuncoesUser()
